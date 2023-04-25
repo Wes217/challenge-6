3b9e06bff0eed0f98cec7a511b88cf7a7e7029cb
Lendo <wesreedbusiness@gmail.com>
2023-04-25 17:15:42 -0500

    History Elements and storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,12 @@ var cardAreaEl = document.querySelector('#cardArea')
 var historyEl = document.querySelector('#history')
 var srhBtnEl = document.querySelector('#srhBtn')
 var srhInputEl = document.querySelector('#srhInput')
+var historyListEl = document.querySelector('#historyList')
 
 var BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast?appid=ed9c4a903d4311b954d0f472b07f5c25&units=imperial&q='
 
+
+var cityHistory = []
 
 
 historyEl.addEventListener('click',function(event){
@@ -13,10 +16,41 @@ historyEl.addEventListener('click',function(event){
     var input = srhInputEl.value
     var term = BASE_URL + input
     getForecast(term)
+    saveHistory(input)
 })
 
+function saveHistory(city){
+    if(cityHistory.includes(city)) return
+    cityHistory.push(city)
+    localStorage.setItem('cityHistory',JSON.stringify(cityHistory))
+    renderHistory()
+}
 
+function renderHistory(){
+    for(var i = 0;i < cityHistory.length;i++){
+    var HistoryItem = createHistoryItemElement(i)
+    historyListEl.appendChild(HistoryItem);
+    }
+    
+}
 
+function createHistoryItemElement(i){
+    var itemEl = document.createElement('div');
+    itemEl.setAttribute('class','d-grid gap-2');
+    var btnEl = document.createElement('button')
+    btnEl.setAttribute('class',"btn btn-primary");
+    btnEl.textContent = cityHistory[i]
+    itemEl.append(btnEl)
+    return itemEl
+}
+
+function historyInit(){
+    var storedCity = JSON.parse(localStorage.getItem('cityHistory'));
+    if(storedCity !== null){
+        cityHistory = storedCity
+    }
+    renderHistory();
+}
 
 
 function getForecast(city){
@@ -39,15 +73,25 @@ function getForecast(city){
 
 
 
-function createCardElement(cast){
-    if (cardAreaEl.hasChildNodes()){
-        cardAreaEl.removeChild(cardAreaEl.firstChild)
-    }
-    
-
-    
+function create3hCardElement(cast){
+    cardAreaEl.innerHTML = '';
     var cardEl = document.createElement('div');
+    cardEl.classList.add("col-12");
+    var titleEl = document.createElement('h2');
+    titleEl.textContent = cast.city.name;
+    var tempEl = document.createElement('h3');
+    tempEl.textContent = 'temp: '+ cast.list[0].main.temp + ' °F'
+    var windEl = document.createElement('h3');
+    windEl.textContent = 'wind: '+ cast.list[0].wind.speed + ' MPH'
+    var humidEl = document.createElement('h3');
+    humidEl.textContent = 'humid: '+ cast.list[0].main.humidity + '%'
+    cardEl.append(titleEl,tempEl,windEl,humidEl);
+    return cardEl;
+}
 
+function create5DCardElement(cast){
+    var cardEl = document.createElement('div');
+    cardEl.classList.add("col-2");
     var titleEl = document.createElement('h2');
     titleEl.textContent = cast.city.name;
     var tempEl = document.createElement('h3');
@@ -62,8 +106,12 @@ function createCardElement(cast){
 
 function renderWeather(cast){
     
-    var weatherDay = createCardElement(cast);
-   
-    cardAreaEl.appendChild(weatherDay);
+    var weather3h = create3hCardElement(cast);
+    var weather5D = create5DCardElement(cast);
+    cardAreaEl.appendChild(weather3h);
+    cardAreaEl.appendChild(weather5D);
 }
 
+
+
+historyInit()
